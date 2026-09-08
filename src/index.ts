9152077e8dbf879435a0021762aea529da25a77b
Developer (Asheem) <developer@Developers-iMac.ht.home>
2026-09-08 18:08:02 -0400

    style: normalize quotes in entry point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { initBioModal } from './modules/bio-modal';
-import { initClock } from './modules/clock';
-import { initParallax } from './modules/parallax';
-import { initRegionMap } from './modules/region-map';
-import { initSmoothScroll } from './modules/smooth-scroll';
-import { initYear } from './modules/year';
+import { initBioModal } from "./modules/bio-modal";
+import { initClock } from "./modules/clock";
+import { initParallax } from "./modules/parallax";
+import { initRegionMap } from "./modules/region-map";
+import { initSmoothScroll } from "./modules/smooth-scroll";
+import { initYear } from "./modules/year";
 
 // Parallax rides Lenis's scroll callback, so it needs the instance. It is
 // undefined under prefers-reduced-motion, which is also the signal to skip.
@@ -16,4 +16,4 @@ initParallax(lenis);
 initBioModal(lenis);
 
 // Release the pre-paint scroll lock set by the head bootstrap.
-document.documentElement.classList.remove('is-loading');
+document.documentElement.classList.remove("is-loading");
